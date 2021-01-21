1467b96310c0c34616a1320bb2bff93cfa7cb8a8
elik <elik@example.com>
2021-01-21 21:56:37 +0200

    Update Database

--- BaraholkaTeamProject.DAL/Migrations/20210121195455_Add column image to table.Designer.cs
+++ BaraholkaTeamProject.DAL/Migrations/20210121195455_Add column image to table.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using BaraholkaTeamProject.DAL.ContextData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BaraholkaTeamProject.DAL.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20210121195455_Add column image to table")]
+    partial class Addcolumnimagetotable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BaraholkaTeamProject.DAL/Migrations/20210121195455_Add column image to table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaraholkaTeamProject.DAL.Migrations
+{
+    public partial class Addcolumnimagetotable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "tblUsersTeamProject",
+                type: "bytea",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "tblUsersTeamProject");
+        }
+    }
+}
